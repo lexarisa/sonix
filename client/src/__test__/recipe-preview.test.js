65d37e6b282eab741d2f95f1283be864f468a6e1
lexarisa <lexarisaa@gmail.com>
2022-05-24 19:06:22 +0200

    test

--- client/src/__test__/recipe-preview.test.js
+++ client/src/__test__/recipe-preview.test.js
@@ -1,11 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../state/store';
 import RecipePreview from '../components/non-auth/recipe-preview';
 
-const customRender = (component) =>
-  render(
+const MockComponent = ({ recipe }) => {
+  return (
     <Provider store={store}>
-      <RecipePreview />
+      <RecipePreview recipe={recipe} />
     </Provider>
   );
+};
+
+describe('Recipe Preview', () => {
+  it('should change like button once clicked', async () => {
+    render(<MockComponent recipe={{}} />);
+    const buttonElement = screen.getByRole('button', { name: /like/i });
+    fireEvent.click(buttonElement, {}); //second arg is what we what to change by
+  });
+
+  it('should be able to play audio', () => {});
+});
